6c1a3b3b2830a368ca8707b66141516f23b9e5ec
calvincetom <calvincetom@outlook.com>
2022-07-19 19:39:55 +0300

    fix requirement fail

--- sort.h
+++ sort.h
@@ -1,17 +1,49 @@
-#ifndef SORT
-#define SORT
+#ifndef SORT_H
+#define SORT_H
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 
 void bubble_sort(int *array, size_t size);
-int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t size, int value);
-int help_binary(int *array, int value, size_t lo, size_t hi);
-void array_print(int *array, size_t lo, size_t hi);
-int jump_search(int *array, size_t size, int value);
-int advanced_binary(int *array, size_t size, int value);
-int exponential_search(int *array, size_t size, int value);
+int len_list(listint_t *h);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void quick_recursion(int *array, int left, int right, size_t size);
+int partition(int *array, int left, int right, size_t size);
+void shell_sort(int *array, size_t size);
+void switch_nodes(listint_t **list, listint_t **p);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void merge_recursion(int *arr, int *array, size_t left, size_t right);
+void merge_subarray(int *arr, int *array, size_t left,
+		size_t middle, size_t right);
+void heap_sort(int *array, size_t size);
+void heapify(int *array, size_t s, size_t root, size_t size);
+void radix_sort(int *array, size_t size);
+void count_sort_LSD(int *array, size_t size, size_t lsd);
+void quick_sort_hoare(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void bitonic_recursion(int *array, int l, int r, int direction, size_t size);
+void bitonic_merge(int *array, int l, int r, int direction);
 
 #endif
